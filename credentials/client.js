1caa9d156a4c86856051df33b0dc921570a511d4
Michal Rosa <michal.rosa@kiwi.com>
2020-03-13 08:01:18 +0100

    inital push

---
+++ credentials/client.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAGRUzY-KHyZsimSLHOCn9fYhge8FgN5qw",
+  authDomain: "crwn-db-e0911.firebaseapp.com",
+  databaseURL: "https://crwn-db-e0911.firebaseio.com",
+  projectId: "crwn-db-e0911",
+  storageBucket: "crwn-db-e0911.appspot.com",
+  messagingSenderId: "143749898701",
+  appId: "1:143749898701:web:160ace1d9ed359b4796484",
+  measurementId: "G-HNRMM23N93"
+};
---
+++ credentials/server.js
@@ -0,0 +1,14 @@
+const serverFirebaseConfig = {
+  type: "service_account",
+  project_id: "crwn-db-e0911",
+  private_key_id: "4a1c13e37668814aea3b44b80cd0d50c6559f266",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDS7Nns50UR0FLq\nZTiQKatnZCD6TgXk/Pc5BFOCPyKGObHbNU+hiVpH3QsJyc5C2XBnHq/hyYSiFZQw\nTr8fiSDQ98/Zv4ArN9eAxjI1q8O4fK/O3hydCFwk4YfnepBE0MjruPNIZIkHev0m\nXNer43cR6Lo61I4t3UITZNygXxVJkLSfXG0DYEpbHANArs+I7jAgPYr+Q0IKJzhG\n7sqQ4kQn0Qoa2P9UwB8cdS33VjT33sotIU4L5asTy6Vp7jFcbf+ffUAg8C9edpE6\nXGusg40TpS1ESNmdM12OOro5mbIXVgyM5AF7fgu6O6qNh8CRNsu0/54ZUXOWr2Xs\nXkYtUfFHAgMBAAECggEAAmj5aScQAiREEPCSJQziK/7Cul7HhiEhKVzJdXyyMMrE\n+Pu9dVuOlVv1uH6+pW3lm7qyiMfBhQvuaF+QoylxH2Ii1QVGpM0btcQjURCtiP9H\nulCNuXpIeeq9H6HQtzCUMyP8XPs6bkRIqIu5XSSZY4d/ZT+w1jOn1x/jviNdU2dX\nZo10yrqgfMyPCyKqM8L7K4Z8ckUn+96JToPGpRELNm/zy6hKvK9PvPH2ce0YMSSy\nBMAbCe7ottA8e9jo+F7eJvp5GgAN0ldnQrXdrzUnk10oUryAc3QEqwsMfzsTFQhI\nFxFGTl0HvcPrkdUGdg5RML9iOdkkwbXi+JpSOlmM+QKBgQDtrAA7usKUqWZj+Flu\nsCvIiUUPCS0rFKjbPz8zHXhXZDCYBME9l97cyutApfQFewfJ3OO6Gp/ypraDzaSY\n+ftQiYx7fqbSlRPIr4tsnb+wnjLMRYLmWRJIVudZupD7yrY+XwwahjVBjGvA5R4L\n86VGkpFCWqerGd17ClNmV56ceQKBgQDjMNSyN6odIjlodfVQED/zEYhrSg/K1Ap3\nyr+pZUaeBxa1VtipapZ5U/onPr0GNitUX7IrQvBf68KyecuflO2wymcGrhWwEQss\njk5E1Hmi9k6loecapyqrrDnzhEBi2QGmoZxnZS/pamGL9SESqFsi4hQARgeIRE5o\nhaOTMtILvwKBgDUXpzWwkqcC8IyEffXsBNvXTkX9lsl44/+WMVgJzUg4aWKMLV7E\n/lP0QHlAmTJgOW0umvCH4hMiisMGvS4vKaTkFS3BLSL9il3aWRr6YkfiDxbysDsk\nyqC+SRASTTDyJ6PnN5z1PhBWffupMv0YgGMzep+IAMyICqHhvJ+wEw6BAoGBALlD\nHo7duYjcWbgHSK2rMprB1162HD6nvomE+dnmzjkbSrdBRJiSZHY3tqu+C2I/VgIN\nrzp78YwmsrPbgeJ2DYW252CZIPJcU6JXwRd7tFzhQazrY4tmefNYFEGxvNdeYmxB\nKUtfRjyvFTCOChQu2XKOrQS0FnxWRyIQqBzmukX9AoGAQE3Kl5A8lhulZgHqCuf6\n3XDfAT50oN1PSxFtEMNvKF7tzPHSzNvIwxVDVDOM+yxYFlu9dCDQ5dSvh3E+glFO\nakW3rh4MJO7zC4jsqgvPWo42tbVFtqT886kA0w/AatAT4wHtWsbJjsVZhn7PhYbZ\n4Yj1Xor/TXysmAn2yjMp1Q4=\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-a9sp6@crwn-db-e0911.iam.gserviceaccount.com",
+  client_id: "116096597921893313311",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-a9sp6%40crwn-db-e0911.iam.gserviceaccount.com"
+};
